19b875eed28a3c3bb6cbed40d01eb7fa574cc121
Fil2k <71265030+FutureMakerfc@users.noreply.github.com>
2022-09-02 14:41:21 +0100

    Add files via upload

---
+++ structPassingFractions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct Fraction
+{
+    int numerator{};
+    int denominator{};
+};
+
+Fraction getFraction()
+{
+    Fraction temp;
+    std::cout << "Enter a value for the numerator: ";
+    std::cin >> temp.numerator;
+    std::cout << "Enter a value for the denominator: ";
+    std::cin >> temp.denominator;
+
+    return temp;
+}
+
+void printFractionMultiplication(const Fraction num1, const Fraction num2)
+{
+    std::cout << "Your fractions multiplied together: " << num1.numerator * num2.numerator << '/' << num1.denominator * num2.denominator;
+}
+
+int main()
+{
+    Fraction fraction1 { getFraction() };
+    Fraction fraction2 { getFraction() };
+    printFractionMultiplication(fraction1, fraction2);
+    return 0;
+}
