6593f9365b7fce54d2638e0289d9f9a002175053
Fil2k <71265030+FutureMakerfc@users.noreply.github.com>
2022-09-02 14:43:24 +0100

    Update structPassingFractions.cpp

--- structPassingFractions.cpp
+++ structPassingFractions.cpp
@@ -2,8 +2,8 @@
 
 struct Fraction
 {
-    int numerator{};
-    int denominator{};
+    int numerator{0};
+    int denominator{1};
 };
 
 Fraction getFraction()
@@ -17,7 +17,7 @@ Fraction getFraction()
     return temp;
 }
 
-void printFractionMultiplication(const Fraction num1, const Fraction num2)
+void printFractionMultiplication(const Fraction& num1, const Fraction& num2)
 {
     std::cout << "Your fractions multiplied together: " << num1.numerator * num2.numerator << '/' << num1.denominator * num2.denominator;
 }
@@ -26,6 +26,7 @@ int main()
 {
     Fraction fraction1 { getFraction() };
     Fraction fraction2 { getFraction() };
+
     printFractionMultiplication(fraction1, fraction2);
     return 0;
 }
